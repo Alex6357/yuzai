70727e4b66842cf16d18e35d5cd498492538fc70
Alex11 <73743056+Alex6357@users.noreply.github.com>
2025-09-15 14:12:23 +0800

    feat(extensions/leveldb): 默认保存位置改为 data/leveldb

--- lib/extensions/leveldb/index.ts
+++ lib/extensions/leveldb/index.ts
@@ -6,7 +6,7 @@ import config from "yuzai/config";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- 数据库存储类型由调用者确定
 const db = new Level<string, any>(
-  config.system.leveldb?.path ?? path.join(config.rootDir, "data", "leveldb", "db"),
+  config.system.leveldb?.path ?? path.join(config.rootDir, "data", "leveldb"),
   { valueEncoding: "json" },
 );
 
